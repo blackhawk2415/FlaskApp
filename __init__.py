81a78f97bb1969598350bca64d3da9c6a371ee4a
blackhawk2415 <jialuji@usc.edu>
2016-08-16 20:53:43 -0700

    basic html pages

--- __init__.py
+++ __init__.py
@@ -1,11 +1,40 @@
-from flask import Flask
-
+from flask import Flask, render_template
+ 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return "Hi there, how ya doin?"
+
+    title = "Epic Tutorials"
+    paragraph = ["wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!","wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!wow I am learning so much great stuff!"]
+
+    try:
+        return render_template("main.html", title = title, paragraph=paragraph)
+    except Exception, e:
+        return str(e)
+
+@app.route('/about')
+def aboutpage():
+
+    title = "About this site"
+    paragraph = ["blah blah blah memememememmeme blah blah memememe"]
+
+    pageType = 'about'
+
+    return render_template("main.html", title=title, paragraph=paragraph, pageType=pageType)
 
 
+@app.route('/about/contact')
+def contactPage():
+
+    title = "About this site"
+    paragraph = ["blah blah blah memememememmeme blah blah memememe"]
+
+    pageType = 'about'
+
+    return render_template("main.html", title=title, paragraph=paragraph, pageType=pageType)
+
+
+ 
 if __name__ == "__main__":
-    app.run()
+	app.run(debug = True, passthrough_errors=True)
